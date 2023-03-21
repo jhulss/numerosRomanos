72c0087ce87868129895187b55f4ed47a8cf6878
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-03-21 02:22:52 -0400

    cuarta prueba con numeros romanos

--- src/romanos.js
+++ src/romanos.js
@@ -7,38 +7,38 @@ function numRomanos(num) {
     if (num == 1)
     {
       numRom = numRom + "I";
-      num =- 1;
+      num -= 1;
     }
     else{
       if(num == 5)
       {
         numRom = numRom + "V";
-        num =- 5;
+        num -= 5;
       }else{
         if(num == 9)
         {
           numRom = numRom + "IX";
-          num =- 9;
+          num -= 9;
         }
         else{
           if (num == 10){
             numRom = numRom + "X";
-            num =- 10;
+            num -= 10;
           }
           else{
             if(num == 50 ){
               numRom = numRom + "L";
-              num =- 50;
+              num -= 50;
             }else{
               if(num == 90){
                 numRom = numRom + "XC";
-                num =- 90;
+                num -= 90;
               }
               else{
                 if(num == 100)
                 {
                   numRom = numRom + "C";
-                  num =- 100;
+                  num -= 100;
                 }
               }
             }
