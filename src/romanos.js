f643bdf76f25aceddc8af7f0e4b0c7c88835630b
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-03-21 02:02:22 -0400

    Pruebas con los numeros romanos

--- src/romanos.js
+++ src/romanos.js
@@ -1,6 +1,53 @@
 function numRomanos(num) {
   var numRom = "";
-  return num ;
+
+  while(num != 0)
+  {
+
+    if (num == 1)
+    {
+      numRom = numRom + "I";
+      num =- 1;
+    }
+    else{
+      if(num == 5)
+      {
+        numRom = numRom + "V";
+        num =- 5;
+      }else{
+        if(num == 9)
+        {
+          numRom = numRom + "IX";
+          num =- 9;
+        }
+        else{
+          if (num == 10){
+            numRom = numRom + "X";
+            num =- 10;
+          }
+          else{
+            if(num == 50 ){
+              numRom = numRom + "L";
+              num =- 50;
+            }else{
+              if(num == 90){
+                numRom = numRom + "XC";
+                num =- 90;
+              }
+              else{
+                if(num == 100)
+                {
+                  numRom = numRom + "C";
+                  num =- 100;
+                }
+              }
+            }
+          }
+        }
+      }
+    } 
+  }
+  return numRom;
   }
   
   export default numRomanos;
--- src/romanos.spec.js
+++ src/romanos.spec.js
@@ -1,7 +1,33 @@
-
+import numRomanos from "./romanos";
 
 describe("Numero romano", () => {
-  it("deberia devolver un numero romano", () => {
+  it("deberia devolver el primer numero romano", () => {
     expect(numRomanos(1)).toEqual("I");
   });
+  
+  it("deberia devolver el numero romano para 5", () => {
+    expect(numRomanos(5)).toEqual("V");
+  });
+
+  it("deberia devolver el numero romano para 9", () => {
+    expect(numRomanos(9)).toEqual("IX");
+  });
+
+  it("deberia devolver el numero romano para 10", () => {
+    expect(numRomanos(10)).toEqual("X");
+  });
+
+  it("deberia devolver el numero romano para 50", () => {
+    expect(numRomanos(50)).toEqual("L");
+  });
+
+  it("deberia devolver el numero romano para 90", () => {
+    expect(numRomanos(90)).toEqual("XC");
+  });
+
+  it("deberia devolver el numero romano para 10", () => {
+    expect(numRomanos(100)).toEqual("C");
+  });
+
+
 });
